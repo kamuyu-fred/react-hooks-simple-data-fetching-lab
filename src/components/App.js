2fd874e584b8a1457a95249e19bd57dbba4169f3
fred <daktarispacetime@gmail.com>
2023-01-16 14:42:07 +0300

    bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    let [image, setImage] = useState("");
+    let [isloaded, setIsLOaded] = useState(false)
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => {
+           return (
+                setImage(data.message),
+                setIsLOaded(true))         
+            })
+    }, [])
+
+    if (isloaded === false) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
+    return (
+        <>
+            <img src={image} alt="A Random Dog"></img>
+        </>        
+    )
+};
+
+    
+
+
+export default App
